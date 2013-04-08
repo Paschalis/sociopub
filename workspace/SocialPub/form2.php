2a19633c15453d0554068da12e145896faa99b42
demetris christofi <dimitros89@gmail.com>
2013-04-09 00:29:44 +0300

    useless commit

--- workspace/SocialPub/form2.php
+++ workspace/SocialPub/form2.php
@@ -1,75 +1,76 @@
 <html>
 <head>
 
-<script type="text/javascript" src="checks.js"</script>
+<!--<script type="text/javascript" src="checks.js"</script>-->
 </head>
 <body>
 <table width="100%">
     <tbody>
     <tr>
-        <td class="normalfont"><span style="font-family: arial,verdana; font-size: 10pt;">Please fill out the form below to register:<br />
+        <td class=""><span style="font-family: arial,verdana; font-size: 10pt;">Please fill out the form below to register:<br />
             <br />
 
             <div>
-                <form method="post"  action="register.php">
+                <form method="post"  action="scripts/register.php">
 
                     <table cellspacing="1" cellpadding="3" border="0">
                         <tbody>
                         <tr>
-                            <td class="normalfont">Username:</td>
+                            <td class="">Username:</td>
                             <td>
                                 <input type="text" class="formInput" name="username" /></td>
                         </tr>
 
                         <tr>
-                            <td class="normalfont">Email:</td>
+                            <td class= "">Email:</td>
                             <td>
                                 <input type="text" class="formInput" name="email" /></td>
                         </tr>
 
                         <tr>
-                            <td class="normalfont">Password:</td>
+                            <td class= "">Password:</td>
                             <td>
                                 <input type="text" class="formInput" name="password" /></td>
                         </tr>
                         <tr>
-                            <td class="normalfont">Confirm Password:</td>
+                            <td class= "">Confirm Password:</td>
                             <td>
-                                <input type="text" class="formInput" name="confirmpassword" /></td>
+                                <input type="text" class="formInput" name="confPassword" /></td>
                         </tr>
                         <tr>
-                            <td class="normalfont">First name:</td>
+                            <td class= "">First name:</td>
                             <td>
-                                <input type="text" class="formInput" name="fname"  /></td>
+                                <input type="text" class="formInput" name="name"  /></td>
                         </tr>
                         <tr>
-                            <td class="normalfont">Last name:</td>
+                            <td class= "">Last name:</td>
                             <td>
-                                <input type="text" class="formInput" name="lname"  /></td>
+                                <input type="text" class="formInput" name="surname"  /></td>
                         </tr>
 
 
                         <tr>
-                            <td class="normalfont">Gender:</td>
+                            <td class= "">Gender:</td>
                             <td>
                                 <input type="text" class="formInput" name="gender" /></td>
                         </tr>
 
                         <tr>
-                            <td class="normalfont">Country:</td>
+                            <td class= "">Country:</td>
                             <td>
                                 <input type="text" class="formInput" name="country" /></td>
                         </tr>
 
 
                         <tr>
-                            <td class="normalfont"></td>
+                            <td class= ""></td>
                             <td>
                                 <input type="submit" class="formButton" value="Register" /></td>
                         </tr>
                         </tbody>
                     </table>
-                </form></div></span></td>
+                </form>
+            </div></span></td>
     </tr>
     </tbody>
 </table>

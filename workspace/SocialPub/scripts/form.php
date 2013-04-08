723991216206120bc4a9ae156cf067b3c59d067e
demetris christofi <dimitros89@gmail.com>
2013-04-08 23:21:11 +0300

    change form2 directory

---
+++ workspace/SocialPub/scripts/form.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+</head>
+<body>
+<table>
+   <div id="labels">
+
+<tr>
+   <td>
+    Username:
+   </td>
+
+</tr>
+
+
+
+
+   </div>
+    <div id="textboxes">
+        <tr>
+
+            <input type="text" class="formInput" name="username" />
+
+
+        </tr>
+
+
+    </div>
+
+</table>
+</body>
+</html>

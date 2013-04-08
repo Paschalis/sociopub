ac26cbd36f8c3ad91580ed3f63636b3751d12e18
Paschalis <paschalis.mpeis@gmail.com>
2013-04-08 18:12:45 +0300

    Added git ignore readme and index for php scripts

--- Readme.md
+++ Readme.md
@@ -1,5 +1,14 @@
 425 Project
 
+PHP:
+.extra PHP code
+
+SQL
+.SQL Code
+
+Webworspace
+.PHP STORM workspace
+
 Paschalis Mpeis
 
 ..edit in future and commit/push your name
---
+++ workspace/PHP/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: paschalis
+ * Date: 4/8/13
+ * Time: 6:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+?>
+
+Home of PHP Scripts.
+
+
+boo!!
+
+

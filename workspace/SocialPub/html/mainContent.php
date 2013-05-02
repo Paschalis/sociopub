a0939d152324e990f93ebc78906e8c4990211bf5
Paschalis <paschalis.mpeis@gmail.com>
2013-05-02 09:11:24 +0300

    Even more beauty!
    + fixed case articles where initially loaded!!
    No more overlaps! ;)

--- workspace/SocialPub/html/mainContent.php
+++ workspace/SocialPub/html/mainContent.php
@@ -171,16 +171,13 @@ if ($_SESSION["loggedin"] == 1) {
                                 + '</div>'
                                 + '<div class="box-body">'
                                 + '<h4>' + datum.title + '</h4>'
-                                + '<div class="articleID" style="display: none">' + datum.uid + '</div>'
-
-                                + '<time class="date timeago" datetime="' + datum.added + '" >' + jQuery.timeago(new Date(datum.added * 1000)) + '</time>'
+                                + '<p class="date timeago" datetime="' + datum.added + '" >' + jQuery.timeago(new Date(datum.added * 1000)) + '</p>'
                                 + '<p>' + datum.description + '</p>'
-
-
                                 + '<div class="readMore"><a href="' + datum.url + '" target="_blank">more...</a></div>'
-                            + '<span class="badge badge-info likes">+' + datum.likes + '</span>'
+                                + '<span class="badge badge-info likes">+' + datum.likes + '</span>'
                                 + '<span class="badge shares" >Shares: ' + datum.shares + '</span>'
                                 + '<span class="badge views" >Views: ' + datum.views + '</span>'
+                                + '<span class="articleID" style="display: none">' + datum.uid + '</span>'
 
                                 + '</div>'
                                 + '</div>';

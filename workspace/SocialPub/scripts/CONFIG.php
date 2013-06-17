d08b3f6bda95fb14bf8c0fad7cdd3e57d9dd8f56
Paschalis Mpeis <paschalis.mpeis@gmail.com>
2013-06-17 18:08:16 +0300

    Add template configuration file

---
+++ workspace/SocialPub/scripts/CONFIG.php
@@ -0,0 +1,31 @@
+<?php
+
+////////// PLEASE FILL ALL BELOW INFORMATION ////////////
+
+// The name of the database
+define('DB_NAME', '');
+
+//Host of the database
+define('DB_HOST', '');
+
+define('DB_DSN', 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME);
+//DB Username
+define('DB_USER', '');
+// Db password
+define('DB_PASSWORD', '');
+
+// Our name
+define('_NAME', 'SocioPub');
+
+// Social pub email
+define('_EMAIL', '');
+
+define('_URL', 'http://sociopub.paschalis.mp');
+
+//Salt for database password protection
+define('_SALT', '');
+
+//Salt for database password protection
+define('_PEPPER', '');
+
+?>
